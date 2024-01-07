5489adc492469c6af067b7f6bb48dd38f78d03bb
Rody Huancas <rodyhuancas.04@gmail.com>
2024-01-07 10:52:13 -0500

    Add: crud complete items

--- src/controllers/item.ts
+++ src/controllers/item.ts
@@ -1,43 +1,51 @@
 import { Request, Response } from 'express';
 import { handleHttp } from '../utils/error.handle';
-import { insertItem } from '../services/item';
+import { deleteCar, getCar, getCars, insertCar, updateCar } from '../services/item';
 
-const getItem = (req: Request, res: Response) => {
+const getItem = async (req: Request, res: Response) => {
     try {
-
+        const { id } = req.params;
+        const response = await getCar(id);
+        const data = response ? response : "NOT_FOUND";
+        res.send(data)
     } catch (e) {
         handleHttp(res, 'ERROR_GET_ITEM');
     }
 }
 
-const getItems = (req: Request, res: Response) => {
+const getItems = async (req: Request, res: Response) => {
     try {
-
+        const response = await getCars();
+        res.send(response)
     } catch (e) {
         handleHttp(res, 'ERROR_GET_ITEMS');
     }
 }
 
-const updateItem = (req: Request, res: Response) => {
+const postItem = async ({ body }: Request, res: Response) => {
     try {
-
-    } catch (e) {
-        handleHttp(res, 'ERROR_UPDATE_ITEM');
-    }
-}
-
-const postItem = async ({body}: Request, res: Response) => {
-    try {
-        const responseItema = await insertItem(body);
+        const responseItema = await insertCar(body);
         res.send(responseItema);
     } catch (e) {
         handleHttp(res, 'ERROR_POST_ITEM', e);
     }
 }
 
-const deleteItem = (req: Request, res: Response) => {
+const updateItem = async (req: Request, res: Response) => {
     try {
+        const { id } = req.params;
+        const response = await updateCar(id, req.body);
+        res.send(response)
+    } catch (e) {
+        handleHttp(res, 'ERROR_UPDATE_ITEM');
+    }
+}
 
+const deleteItem = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const response = await deleteCar(id);
+        res.send(response)
     } catch (e) {
         handleHttp(res, 'ERROR_DELETE_ITEM');
     }
--- src/services/item.ts
+++ src/services/item.ts
@@ -1,9 +1,37 @@
 import { Car } from '../interfaces/car.interface';
 import ItemModel from '../models/item';
 
-const insertItem = async (item: Car) => {
+const insertCar = async (item: Car) => {
     const responseInsert = await ItemModel.create(item);
     return responseInsert;
 }
 
-export { insertItem }
+const getCars = async () => {
+    const responseItem = await ItemModel.find({});
+    return responseItem;
+}
+
+const getCar = async (id: string) => {
+    const responseItem = await ItemModel.findOne({ _id: id });
+    return responseItem;
+}
+
+const updateCar = async (id: string, data: Car) => {
+    const responseItem = await ItemModel.findOneAndUpdate(
+        { 
+            _id: id 
+        }, 
+        data,
+        {
+            new: true,
+        }
+    );
+    return responseItem;
+}
+
+const deleteCar = async (id: string) => {
+    const responseItem = await ItemModel.deleteOne({_id: id});
+    return responseItem;
+}
+
+export { insertCar, getCars, getCar, updateCar, deleteCar }
