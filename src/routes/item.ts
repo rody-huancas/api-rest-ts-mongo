3d6bfc13071942a6f64ec8a2b487fdd4c44e96f0
Rody Huancas <rodyhuancas.04@gmail.com>
2024-02-10 22:04:25 -0500

    Add: remove middleware of item

--- src/routes/item.ts
+++ src/routes/item.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { deleteItem, getItem, getItems, postItem, updateItem } from "../controllers/item";
-import { logMiddleware } from "../middleware/log";
+// import { logMiddleware } from "../middleware/log";
 
 const router = Router();
 
@@ -8,7 +8,7 @@ const router = Router();
 * https://localhost:3000/items [GET]
 */
 router.get("/", getItems);
-router.get("/:id", logMiddleware, getItem);
+router.get("/:id", getItem);
 router.post("/", postItem);
 router.put("/:id", updateItem);
 router.delete("/:id", deleteItem);
